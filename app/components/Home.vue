61219bf613d558de3f9eb77a06a3c17c31f2fb41
Chiche Dickson <immicks@gmail.com>
2019-11-06 13:57:27 +0100

    Actual first commit

---
+++ app/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar">
+      <Label class="action-bar-title" text="Home"></Label>
+    </ActionBar>
+    <StackLayout></StackLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+  computed: {
+    message() {
+      return "Blank {N}-Vue app";
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+// Start custom common variables
+@import "../app-variables";
+// End custom common variables
+
+// Custom styles
+.fa {
+  color: $accent-dark;
+}
+
+.info {
+  font-size: 20;
+}
+</style>
